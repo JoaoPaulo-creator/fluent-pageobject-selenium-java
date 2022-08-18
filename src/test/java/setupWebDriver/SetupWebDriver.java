a65dc25aab7ede0ad1ec122685aa5e5c8703ba13
joao <joao@pop-os.localdomain>
2022-08-17 23:07:52 -0300

    Removido imports não utilizado + apagado código comentado

--- src/test/java/setupWebDriver/SetupWebDriver.java
+++ src/test/java/setupWebDriver/SetupWebDriver.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
 
 public abstract class SetupWebDriver {
 
@@ -34,38 +33,5 @@ public abstract class SetupWebDriver {
     void teardown() {
         driver.quit();
     }
-
-//    @Before
-//    public void setupTeste(){
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/" + getOperatingSystem());
-//
-//        driver = new ChromeDriver();
-//        driver.navigate().to("https://buscacepinter.correios.com.br/app/endereco/index.php");
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--incognito");
-//    }
-//
-//    @After
-//    public void tearDown(){
-//        driver.close();
-//        driver.quit();
-//    }
-//
-//    private String getOperatingSystem() {
-//        String operatingSystem = System.getProperty("os.name");
-//        String webDriver = "";
-//
-//        if(operatingSystem.equals("Linux")){
-//            webDriver = "chromedriver";
-//        }
-//        if(operatingSystem.equals("Windows")){
-//            webDriver = "chromedriver.exe";
-//        }
-//        return webDriver;
-//    }
-
 
 }
