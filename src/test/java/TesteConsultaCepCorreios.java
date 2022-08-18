013823cc035cdd9dd0a7c8a227cb2679ffe412c3
joao <joao@pop-os.localdomain>
2022-08-17 23:06:14 -0300

    Adicionado WebDriverManager ao projeto + removido código antigo + atualizado versão do junit + adicionado dependência do WebDriverManager

--- src/test/java/TesteConsultaCepCorreios.java
+++ src/test/java/TesteConsultaCepCorreios.java
@@ -1,5 +1,6 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 import pages.ConsultaCepPage;
 import setupWebDriver.SetupWebDriver;
 
--- src/test/java/setupWebDriver/SetupWebDriver.java
+++ src/test/java/setupWebDriver/SetupWebDriver.java
@@ -1,7 +1,9 @@
 package setupWebDriver;
 
-import org.junit.After;
-import org.junit.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,37 +14,58 @@ public abstract class SetupWebDriver {
 
     protected WebDriver driver;
 
-    @Before
-    public void setupTeste(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/" + getOperatingSystem());
-
-        driver = new ChromeDriver();
-        driver.navigate().to("https://buscacepinter.correios.com.br/app/endereco/index.php");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
-    @After
-    public void tearDown(){
-        driver.close();
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://buscacepinter.correios.com.br/app/endereco/index.php");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+
+    }
+
+    @AfterEach
+    void teardown() {
         driver.quit();
     }
 
-    private String getOperatingSystem() {
-        String operatingSystem = System.getProperty("os.name");
-        String webDriver = "";
-
-        if(operatingSystem.equals("Linux")){
-            webDriver = "chromedriver";
-        }
-        if(operatingSystem.equals("Windows")){
-            webDriver = "chromedriver.exe";
-        }
-        return webDriver;
-    }
+//    @Before
+//    public void setupTeste(){
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/" + getOperatingSystem());
+//
+//        driver = new ChromeDriver();
+//        driver.navigate().to("https://buscacepinter.correios.com.br/app/endereco/index.php");
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--incognito");
+//    }
+//
+//    @After
+//    public void tearDown(){
+//        driver.close();
+//        driver.quit();
+//    }
+//
+//    private String getOperatingSystem() {
+//        String operatingSystem = System.getProperty("os.name");
+//        String webDriver = "";
+//
+//        if(operatingSystem.equals("Linux")){
+//            webDriver = "chromedriver";
+//        }
+//        if(operatingSystem.equals("Windows")){
+//            webDriver = "chromedriver.exe";
+//        }
+//        return webDriver;
+//    }
 
 
 }
